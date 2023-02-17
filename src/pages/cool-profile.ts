97b603160e9b42d020dd3136a443905f66144d14
okikio <okikio.dev@gmail.com>
2023-02-17 00:22:21 -0500

    3 files modified

--- src/pages/cool-profile.ts
+++ src/pages/cool-profile.ts
@@ -1,9 +1,30 @@
 import type { APIContext } from 'astro';
+import { writeFile } from 'fs/promises';
+
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(__filename);
+console.log(__dirname);
 
 // File routes export a get() function, which gets called to generate the file.
 // Return an object with `body` to save the file contents in your final build.
 export async function post({ params, request }: APIContext) {
   const formData = await request.formData();
+  await Promise.all(
+    formData
+      .getAll('files')
+      .map(async (file: File) =>
+        writeFile(
+          resolve(__dirname, 'tmp', file.webkitRelativePath ?? file.name),
+          new Uint8Array(await file.arrayBuffer())
+        )
+      )
+  );
+
   return {
     body: JSON.stringify({
       fileNames: await Promise.all(
